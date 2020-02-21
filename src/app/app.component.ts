4dd2a320712114bdf23072abab503883344624a4
Lakshay Chopra (Industrial La329130 Engineering Services (ILa329130ES)) <LA329130@wipro.com>
2020-02-21 16:19:30 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AppService } from './app.service';
 import { Subscription } from 'rxjs';
+import * as XLSX from 'xlsx';
+
+import { AppService } from './app.service';
 
 @Component({
   selector: 'ld-root',
@@ -8,23 +10,79 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  
+
   devices: any;
   deviceSubscription: Subscription;
 
-  constructor(private appService: AppService) {}
+  fileName = 'SilentDevicesSheet.xlsx';
+  fileToUpload: File = null;
+
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
-      this.deviceSubscription = this.appService.getDevicesDetails()
+    this.deviceSubscription = this.appService.getDevicesDetails()
       .subscribe(data => {
-        if(data) {
+        if (data) {
           this.devices = data;
         }
       });
   }
 
+  exportexcel(): void {
+    /* table id is passed over here */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+  }
+
+  onFileChange(ev) {
+    let workBook = null;
+    let jsonData = null;
+    let tempArray = [];
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        tempArray = XLSX.utils.sheet_to_json(sheet);
+        this.devices = tempArray.map(el => {
+          if (el.hasOwnProperty("Product Number")) {
+            el.pNo = el["Product Number"];
+            delete el["Product Number"];
+          }
+          if (el.hasOwnProperty("Partner")) {
+            el.partner = el["Partner"];
+            delete el["Partner"];
+          }
+          if (el.hasOwnProperty("MS Contract ID")) {
+            el.contractId = el["MS Contract ID"];
+            delete el["MS Contract ID"];
+          }
+          if (el.hasOwnProperty("Last Modified Date")) {
+            el.lastmodifieddate = el["Last Modified Date"];
+            delete el["Last Modified Date"];
+          }
+          return el;
+        });
+        console.log(this.devices);
+        return initial;
+      }, {});
+      console.log(jsonData.Sheet1);
+    }
+    reader.readAsBinaryString(file);
+  }
+
   ngOnDestroy() {
-      this.deviceSubscription.unsubscribe();
+    this.deviceSubscription.unsubscribe();
   }
 
 }
